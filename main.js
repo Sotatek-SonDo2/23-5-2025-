af171bd2648430fe0210562841a8f40021596085
Sotatek-SonDo2 <son.do2@sotatek.com>
2025-05-23 12:01:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//HTML DOM
+
+// var headingNode = document.querySelectorAll('.box .heading-2');
+
+// console.log(headingNode);
+
+// console.log(document.anchors);
+
+var boxNode = document.querySelectorAll('.box-1 li');
+
+console.log(listItemNodes);
